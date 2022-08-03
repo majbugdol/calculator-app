d11c4744585125ba2392d1baaa31dbabdabb9c7b
majbugdol <majbugdol@gmail.com>
2022-08-03 21:18:05 +0200

    adds routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalcFirstThemeComponent } from './calc-first-theme/calc-first-theme.component';
+import { CalcSecondThemeComponent } from './calc-second-theme/calc-second-theme.component';
+import { CalcThirdThemeComponent } from './calc-third-theme/calc-third-theme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'theme-1',
+    component: CalcFirstThemeComponent,
+    title: 'Calculator - first theme',
+  },
+  {
+    path: 'theme-2',
+    component: CalcSecondThemeComponent,
+    title: 'Calculator - second theme',
+  },
+  {
+    path: 'theme-3',
+    component: CalcThirdThemeComponent,
+    title: 'Calculator - third theme',
+  },
+  { path: '', redirectTo: '/theme-1', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
